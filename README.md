b6fd247210b153c1f3534e81552eeb57f5d75bdb
daisy.imbing@gmail.com <email>
2023-10-14 20:05:43 +0800

    Commit all files

---
+++ README.md
@@ -0,0 +1,2 @@
+# MusicPlayer
+Your site is live at https://ikenncode.github.io/MusicPlayer/
---
+++ js/music-list.js
@@ -0,0 +1,57 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [ 
+    {
+      name: "Wide Open Arms",
+      artist: "Marc Scibilia",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Thinking out Loud",
+      artist: "Ed Sheeran",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "Hope It's Too Late",
+      artist: "Michael Bolton",
+      img: "music-8",
+      src: "music-8"
+    },
+    {
+      name: "Lean On Me",
+      artist: "Music Travel Love (Iligan City, Philippines) Bill Withers Cover",
+      img: "music-9",
+      src: "music-9"
+    },
+    {
+      name: "Write This Down",
+      artist: "George Strait",
+      img: "music-10",
+      src: "music-10"
+    },
+    {
+      name: "Check Yes Or No",
+      artist: "George Strait",
+      img: "music-11",
+      src: "music-11"
+    },
+  
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
